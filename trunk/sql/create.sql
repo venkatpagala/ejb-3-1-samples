77467f1678ab1e5800c5d4543270aa03c1b69bc6
nishi.in@gmail.com <nishi.in@gmail.com@f9976e4e-9cfc-ef51-4b78-cd0424c9bcc5>
2011-12-14 19:44:06 +0000

    Simplified DB Table structure/relationships.

--- trunk/sql/create.sql
+++ trunk/sql/create.sql
@@ -1,74 +1,56 @@
-DROP TABLE IF EXISTS `organization`.`billing_details`;
-CREATE TABLE  `organization`.`billing_details` (
-  `emp_id` int(10) unsigned NOT NULL,
-  `org_id` int(10) unsigned NOT NULL,
+delimiter $$
+
+CREATE DATABASE `organization` /*!40100 DEFAULT CHARACTER SET latin1 */$$
+
+delimiter $$
+
+CREATE TABLE `billing_details` (
+  `billing_id` int(10) NOT NULL,
   `per_hour_billing` float NOT NULL,
-  `enabled` tinyint(1) NOT NULL,
-  `contractor` tinyint(1) NOT NULL,
-  `contractor_id` int(10) unsigned NOT NULL,
-  `billing_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY  USING BTREE (`billing_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `is_active` tinyint(1) NOT NULL,
+  `is_contractor` tinyint(1) NOT NULL,
+  `contractor_name` varchar(100) DEFAULT NULL,
+  `emp_id` int(10) NOT NULL,
+  PRIMARY KEY (`billing_id`) USING BTREE,
+  KEY `billing_emp_FK` (`emp_id`),
+  CONSTRAINT `billing_emp_FK` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
 
+delimiter $$
 
-DROP TABLE IF EXISTS `organization`.`employee`;
-CREATE TABLE  `organization`.`employee` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+CREATE TABLE `employee` (
+  `emp_id` int(10) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) NOT NULL,
   `middle_name` varchar(100) NOT NULL,
   `last_name` varchar(45) NOT NULL,
-  `age` int(10) unsigned NOT NULL,
+  `age` int(10) NOT NULL,
   `joining_date` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `org_id` int(10) NOT NULL,
+  `billing_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`emp_id`),
+  KEY `emp_org_FK` (`org_id`),
+  KEY `emp_billing_FK` (`billing_id`),
+  CONSTRAINT `emp_billing_FK` FOREIGN KEY (`billing_id`) REFERENCES `billing_details` (`billing_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `emp_org_FK` FOREIGN KEY (`org_id`) REFERENCES `organization` (`org_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
 
 
-DROP TABLE IF EXISTS `organization`.`organization`;
-CREATE TABLE  `organization`.`organization` (
-  `org_id` int(10) unsigned NOT NULL auto_increment,
+
+delimiter $$
+
+CREATE TABLE `organization` (
+  `org_id` int(10) NOT NULL AUTO_INCREMENT,
   `short_name` varchar(45) NOT NULL,
   `registered_name` varchar(100) NOT NULL,
   `addredd_city` varchar(45) NOT NULL,
   `address_state` varchar(2) NOT NULL,
   `address_country` varchar(45) NOT NULL,
-  PRIMARY KEY  (`org_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-set foreign_key_checks = 0 ; 
-
--- new
-
-ALTER TABLE `organization`.`organization_employee_billing` 
-  ADD CONSTRAINT `billing_FK`
-  FOREIGN KEY org_FK(org_id)
-  REFERENCES `organization`.`billing_details` (billing_id)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
-, ADD INDEX `billing_FK` (billing_id) ;
-
-ALTER TABLE `organization`.`organization_employee_billing` ADD CONSTRAINT UNIQUE org_emp_biln_UQ (billing_id, emp_id, org_id);
-
-ALTER TABLE `organization`.`employee` 
-  ADD CONSTRAINT `emp_org_FK`
-  FOREIGN KEY emp_org_FK(org_id)
-  REFERENCES `organization`.`organization` (org_id)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
-, ADD INDEX `emp_org_FK` (org_id) ;
-
-delimiter $$
-
-CREATE TABLE `organization_employee_billing` (
-  `billing_id` int(10) NOT NULL,
-  `emp_id` int(10) NOT NULL,
-  `org_id` int(10) NOT NULL,
-  UNIQUE KEY `org_emp_biln_UQ` (`billing_id`,`emp_id`,`org_id`),
-  KEY `emp_FK` (`emp_id`),
-  KEY `org_FK` (`org_id`),
-  KEY `billing_FK` (`billing_id`),
-  CONSTRAINT `billing_FK` FOREIGN KEY (`org_id`) REFERENCES `billing_details` (`billing_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `emp_FK` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `org_FK` FOREIGN KEY (`org_id`) REFERENCES `organization` (`org_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`org_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+
+
+
+
 
 
