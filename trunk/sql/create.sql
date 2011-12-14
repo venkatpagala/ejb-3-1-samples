85491d2f333d31e940a479df58cd8224924cc23d
nishi.in@gmail.com <nishi.in@gmail.com@f9976e4e-9cfc-ef51-4b78-cd0424c9bcc5>
2011-12-14 19:41:15 +0000

    Added Constraint instructions for Mac.

--- trunk/sql/create.sql
+++ trunk/sql/create.sql
@@ -33,3 +33,42 @@ CREATE TABLE  `organization`.`organization` (
   `address_country` varchar(45) NOT NULL,
   PRIMARY KEY  (`org_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+set foreign_key_checks = 0 ; 
+
+-- new
+
+ALTER TABLE `organization`.`organization_employee_billing` 
+  ADD CONSTRAINT `billing_FK`
+  FOREIGN KEY org_FK(org_id)
+  REFERENCES `organization`.`billing_details` (billing_id)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `billing_FK` (billing_id) ;
+
+ALTER TABLE `organization`.`organization_employee_billing` ADD CONSTRAINT UNIQUE org_emp_biln_UQ (billing_id, emp_id, org_id);
+
+ALTER TABLE `organization`.`employee` 
+  ADD CONSTRAINT `emp_org_FK`
+  FOREIGN KEY emp_org_FK(org_id)
+  REFERENCES `organization`.`organization` (org_id)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `emp_org_FK` (org_id) ;
+
+delimiter $$
+
+CREATE TABLE `organization_employee_billing` (
+  `billing_id` int(10) NOT NULL,
+  `emp_id` int(10) NOT NULL,
+  `org_id` int(10) NOT NULL,
+  UNIQUE KEY `org_emp_biln_UQ` (`billing_id`,`emp_id`,`org_id`),
+  KEY `emp_FK` (`emp_id`),
+  KEY `org_FK` (`org_id`),
+  KEY `billing_FK` (`billing_id`),
+  CONSTRAINT `billing_FK` FOREIGN KEY (`org_id`) REFERENCES `billing_details` (`billing_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `emp_FK` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `org_FK` FOREIGN KEY (`org_id`) REFERENCES `organization` (`org_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+
