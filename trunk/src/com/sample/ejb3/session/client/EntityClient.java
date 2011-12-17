99e676593e1f0d16b903e0b26afe5da24eaf3b91
nishi.in@gmail.com <nishi.in@gmail.com@f9976e4e-9cfc-ef51-4b78-cd0424c9bcc5>
2011-12-17 21:08:20 +0000

    Create Org. Use LIKE query. Use IN(...) for InnerJoin query.

--- trunk/src/com/sample/ejb3/session/client/EntityClient.java
+++ trunk/src/com/sample/ejb3/session/client/EntityClient.java
@@ -1,38 +1,85 @@
 package com.sample.ejb3.session.client;
 
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import com.sample.ejb3.Constants;
-import com.sample.ejb3.employee.facade.EmployeeFacade;
+import com.sample.ejb3.entity.Employee;
+import com.sample.ejb3.entity.Organization;
+import com.sample.ejb3.facade.employee.EmployeeFacade;
+import com.sample.ejb3.facade.organization.OrganizationFacade;
 
 public class EntityClient {
 
 	private static InitialContext ctx;
 
-	/**
-	 * @param args
-	 * @throws NamingException 
-	 */
-	public static void main(String[] args) throws NamingException {
-		EmployeeFacade employeeBean;
-		
-		 ctx = new InitialContext();
+	private EmployeeFacade employeeBean;
+
+	private OrganizationFacade organizationBean;
+
+	public EntityClient() {
 		try {
+
+			ctx = new InitialContext();
 			employeeBean = (EmployeeFacade) ctx
 					.lookup(Constants.BUSINESS_REMOTE_EMPLOYEE);
 
-			System.out.println("Value "
-					+ employeeBean.add("firstName", "middleName", "lastName",
-							25, GregorianCalendar.getInstance()));
-
+			organizationBean = (OrganizationFacade) ctx
+					.lookup(Constants.BUSINESS_REMOTE_ORGANIZATION);
 		} catch (NamingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * @param args
+	 * @throws NamingException
+	 */
+	public static void main(String[] args) throws NamingException {
+		EntityClient client = new EntityClient();
+		//client.removeAllEmployees();
+
+		Employee emp = client.createEmployee();
+		emp = client.createEmployee();
+		//client.removeEmployee(new Employee(emp.getId() - 1, "null"));
+		List<Employee> employeesInResult = client.findEmployeesByName("ame");
+		// for (Employee e : employeesInResult)
+		// System.out.println("Found emp: " + e.getFirstName());
+
+		List<Organization> orgs = client.getOrgsWithChildLabor();
+
+		System.out.println(orgs.get(0).getName() + orgs.get(0).getOrgId());
+
+		for (Organization o : orgs)
+			System.out.println("Found Org with child labors: " + o.getOrgId()
+					+ ", Name" + o.getName());
 
 	}
 
+	private List<Organization> getOrgsWithChildLabor() {
+		return organizationBean.getOrgsWithUnderageEmployees();
+	}
+
+	public Employee createEmployee() {
+
+		return employeeBean.add("firstName", "middleName", "lastName", 25,
+				GregorianCalendar.getInstance());
+	}
+
+	public void removeEmployee(Employee emp) {
+		employeeBean.remove(emp.getId());
+
+	}
+
+	public void removeAllEmployees() {
+		employeeBean.removeAll();
+	}
+
+	public List<Employee> findEmployeesByName(String name) {
+		return employeeBean.findByName(name);
+	}
 }
